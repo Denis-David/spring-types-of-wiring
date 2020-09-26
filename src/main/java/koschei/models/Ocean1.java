995cbd3748a983a76ea0a23eb3130b647b7834c9
Denis <denidavid777@yandex.ru>
2020-09-26 22:24:20 +0300

    first commit

--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -1,16 +1,17 @@
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Ocean1 {
 
     @Autowired
-    private Island2 island;
+    private Island2 island2;
 
     @Override
     public String toString() {
-        return "на океане остров " + island.toString();
+        return "на океане остров " + island2.toString();
     }
 }
